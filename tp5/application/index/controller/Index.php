fcc14844360e5f526848f31a78f13fbcc1ce9ad6
zhongGangJun <2337078015@qq.com>
2018-09-21 21:34:18 +0800

    9-21

--- tp5/application/index/controller/Index.php
+++ tp5/application/index/controller/Index.php
@@ -63,22 +63,98 @@ class Index extends Controller
 //             shouhuo_tel char(11)
 //              )engine=myisam default charset=utf8";
 //        Db::query($sql);
+         $b = session("admin_user");
+        if(!empty($b)){
+            $this->assign("flag","xinxi");
+            return $this->fetch("index/gongneng");
+        }
         return $this->fetch("index");
     }
 
     function loginPro(){
         $b = Db::table("tb_admin")->where('admin_user',input("username"))->where("admin_pass",input('password'))->find();
         if(!empty($b)){
+            session("admin_user",$b['admin_user']);
+            session("admin_pass",$b['admin_pass']);
            return $this->success("登录成功","Index/gongneng");
         }else{
             return $this->error("登录失败,重新登录","Index/index");
         }
     }
     function gongneng(){
-         $flag = input("flag");
+        $flag = input("flag");
         $this->assign("flag",$flag);
+        switch ($flag){
+            case "cyxxcx":
+                $data = input("post.");
+                $res = null;
+              if(!empty($data)){
+                  $res =  $this->selectCYXX($data);
+              }
+              $this->assign("res",$res);
+                break;
+            case "fhd":
+                $data = input("post.");
+                $this->assign("fahuo",null);
+                if(!empty($data)){
+                    $b = Db::table("tb_shopping")->insert($data);
+                    $this->assign("fahuo",$b);
+                }
+                break;
+            case "fhdcx":
+                $vo = null;
+                $data = input("post.");
+                $da = input("get.");
+                if(!empty($data)){
+                    if($data['select'] == 'fahuo_user'){
+                        $vo =  Db::table("tb_shopping")->where("fahuo_user",$data['key'])->find();
+                    }else{
+                        $vo =  Db::table("tb_shopping")->where("fahuo_id",$data['key'])->find();
+                    }
+                    $this->assign("vo",$vo);
+                }else if(!empty($da)){
+                    Db::table("tb_shopping")->where("fahuo_id",$da['fahuo_id'])->delete();
+                }
+                break;
+        }
         return $this->fetch();
     }
+
+    function  loginout(){
+        session("admin_user",null);
+        session("admin_pass",null);
+        $this->success("注销成功","index/index");
+    }
+    function updatePass(){
+        $b = Db::table("tb_admin")->where("admin_user",input("admin_user"))->update(["admin_pass"=>input("pass")]);
+        if(!empty($b)){
+            $this->success("修改成功","Index/gongneng");
+        }else{
+            $this->error("修改失败","Index/gongneng");
+        }
+    }
+    //确认用户名和密码匹配
+    function querenUser(){
+        $data = input("post.");
+        $b = Db::table("tb_admin")->where("admin_user",$data['admin_user'])->where("admin_pass",$data['admin_pass'])->find();
+        if(!empty($b)){
+//            密码正确
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+    //查询车源信息
+    function selectCYXX($data){
+        $go = $data['go'];
+        $to = $data['to'];
+        $res = Db::table("tb_car")->where("car_road like '%$go%' ")->where("car_road like '%$to%' ")->select();
+        return $res;
+    }
+
+
+
+
 
 
 
